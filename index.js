e1ea8925597f798747393205e213bef72ced2cb9
yuhsuan lee <yuhsuanlee94@gmail.com>
2025-03-31 22:54:11 -0400

    Ajax Call practice

--- index.js
+++ index.js
@@ -21,3 +21,36 @@ document.addEventListener("scroll", () => {
   const positionY = window.scrollY;
   console.log(positionY);
 });
+
+// Get the date with ajax
+const url = "https://swapi.dev/api/people";
+
+// jQuery ajax
+$.ajax({ 
+  type: 'GET',
+  url: url,
+  success: function(response) {
+    console.log('jquery ajax', response);
+  },
+  error: function(error) {
+    console.log(error);
+  }
+}); 
+
+// HMLHttpRequest
+
+const req = new XMLHttpRequest();
+
+req.addEventListener('readystatechange', () => {
+  if (req.readyState === 4) {
+    console.log('xhttp', req.responseText);
+  }
+});
+
+req.open('GET', url);
+req.send();
+
+// 3. fetch method
+fetch(url)
+  .then(resp=>resp.json()).then(data=>console.log('fetch', data))
+  .catch(err=>console.log(err));
