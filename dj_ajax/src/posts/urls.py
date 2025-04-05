bef57ec4f96b56d7fb61f1578070c1232edab910
yuhsuan lee <yuhsuanlee94@gmail.com>
2025-04-05 15:33:35 -0400

    feat: Writing Update and Delete Views

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -5,8 +5,8 @@ from .views import (
   like_unlike_post,
   post_detail,
   post_detail_data_view,
-
-  hello_world_view,
+  delete_post,
+  update_post,
 )
 
 app_name = 'posts'
@@ -16,7 +16,8 @@ urlpatterns = [
   path('like-unlike/', like_unlike_post, name='like-unlike'),
   path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
   path('<pk>/', post_detail, name='post-detail'),
+  path('<pk>/update/', update_post, name='post-update'),
+  path('<pk>/delete/', delete_post, name='post-delete'),
 
-  path('hello-world/', hello_world_view, name='hello-world-view'),
   path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
 ]
