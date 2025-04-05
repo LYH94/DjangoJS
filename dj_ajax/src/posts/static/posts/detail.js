cdf89ed927b5cdf7a4e85c842793d5f1ea0fb3a7
yuhsuan lee <yuhsuanlee94@gmail.com>
2025-04-05 15:14:01 -0400

    feat: Display post title and body on detail page

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -1,3 +1,4 @@
+const postBox = document.getElementById('post-box');
 const backBtn = document.getElementById('back-btn');
 const updateBtn = document.getElementById('update-btn');
 const deleteBtn = document.getElementById('delete-btn');
@@ -22,6 +23,18 @@ $.ajax({
       updateBtn.classList.remove('d-none');
       deleteBtn.classList.remove('d-none');
     }
+
+    const titleEl = document.createElement('h3');
+    titleEl.setAttribute('class', 'mt-3');
+
+    const bodyEl = document.createElement('p');
+    bodyEl.setAttribute('class', 'mt-1');
+
+    titleEl.textContent = data.title;
+    bodyEl.textContent = data.body;
+
+    postBox.appendChild(titleEl);
+    postBox.appendChild(bodyEl);
 
     spinnerBox.classList.add('d-none');
   },
