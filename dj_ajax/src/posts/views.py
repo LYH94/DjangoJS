7e1027a50caadbba3304d41e82653aadc2d0d9d8
yuhsuan lee <yuhsuanlee94@gmail.com>
2025-04-05 21:44:13 -0400

    feat: Implement login required for views or update and delete

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -4,8 +4,10 @@ from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
 from .utils import action_permission
+from djanjo.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def post_list_and_create(request):
   form = PostForm(request.POST or None)
   # qs = Post.objects.all()
@@ -30,6 +32,7 @@ def post_list_and_create(request):
 
   return render(request, 'posts/main.html', context)
 
+@login_required
 def post_detail(request, pk):
   obj = Post.objects.get(pk=pk)
   form = PostForm(request.POST or None, instance=obj)
@@ -41,6 +44,7 @@ def post_detail(request, pk):
 
   return render(request, 'posts/detail.html', context)
 
+@login_required
 def load_post_data_view(request, num_posts):
   if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
     visible = 3
@@ -62,6 +66,7 @@ def load_post_data_view(request, num_posts):
       data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size': size})
 
+@login_required
 def post_detail_data_view(request, pk):
     obj = Post.objects.get(pk=pk)
     data = {
@@ -72,7 +77,8 @@ def post_detail_data_view(request, pk):
       'logged_in': request.user.username,
     }
     return JsonResponse({'data': data})
-    
+
+@login_required
 def like_unlike_post(request):
   if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
     pk = request.POST.get('pk')
@@ -85,6 +91,8 @@ def like_unlike_post(request):
       obj.liked.add(request.user)
     return JsonResponse({'liked': liked, 'count': obj.like_count})
 
+@login_required
+@action_permission
 def update_post(request, pk):
   obj = Post.objects.get(pk=pk)
   if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
@@ -98,6 +106,7 @@ def update_post(request, pk):
       'body': obj.body,
     })
 
+@login_required
 @action_permission
 def delete_post(request, pk):
   obj = Post.objects.get(pk=pk)
@@ -106,6 +115,7 @@ def delete_post(request, pk):
     return JsonResponse({'msg': 'Some message'})
   return JsonResponse({'msg': 'access denied -ajax only'})
 
+@login_required
 def image_upload_view(request):
   if request.method == 'POST':
     img = request.FILES.get('file')
